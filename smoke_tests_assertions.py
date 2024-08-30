d9c657339868f0a4d7ed7d4d23f7c13b2582fd2b
ysgiraldo <ygiraldoca@unal.edu.co>
2024-08-30 12:48:01 -0500

    Se agrega un reporte más automatizado con HTML

--- smoke_tests_assertions.py
+++ smoke_tests_assertions.py
@@ -1,5 +1,6 @@
 from unittest import TestLoader, TestSuite
-from pyunitreport import HTMLTestRunner #para generar el reporte
+# from pyunitreport import HTMLTestRunner #para generar el reporte
+from HtmlTestRunner import HTMLTestRunner # Para generar los reportes en conjunto
 from assertions import AssertionsTest
 from search_tests_assertions import SearchTests
 
@@ -9,15 +10,22 @@ search_tests = TestLoader().loadTestsFromTestCase(SearchTests)
 #contruimos la suite de pruebas
 smoke_test = TestSuite([assertions_test, search_tests])
 
-
 #para generar los reporters
+# kwargs = {
+#     "output": 'smoke-report'
+#     }
 kwargs = {
-    "output": 'smoke-report'
-    }
+    "output": "reports/smoke-report",
+    "report_name": "smoke-report",
+    "combine_reports": True,
+    "add_timestamp": False
+}
 
 #la variable runner almacena un reporte generado por HTMLTestRuner
 #usa como argumento "kwarsp"
+# runner = HTMLTestRunner(**kwargs)
 runner = HTMLTestRunner(**kwargs)
+# runner.run(smoke_test)
 
 #corro el rurner con la suite de prueba
 runner.run(smoke_test) 
